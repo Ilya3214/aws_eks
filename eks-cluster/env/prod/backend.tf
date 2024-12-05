d1d78c7eefe1509b556ba3e4ccb3dd34012ad47a
ilya3214 <ilpredko@gmail.com>
2024-12-05 15:27:46 -0600

    fixed backend

--- eks-cluster/env/prod/backend.tf
+++ eks-cluster/env/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-ilya3214-backend"
-    key            = "terraform/state/aws-eks-cluster-prod/terraform.tfstate"
+    key            = "terraform/tfstate/aws-eks-cluster-prod/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform_lock_table"
     encrypt        = true
