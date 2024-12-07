46328e14bea6d03d6a358378c9504cb77b057252
ilya3214 <ilpredko@gmail.com>
2024-12-06 19:06:06 -0600

    additional modules

--- terraform_infra/env/dev/main.tf
+++ terraform_infra/env/dev/main.tf
@@ -4,3 +4,28 @@ module "vpc" {
   public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   environment = var.environment
 }
+module "security_group" {
+  source      = "../../modules/SG"
+  name        = "docker-sg"
+  description = "Allow inbound traffic for Docker"
+  vpc_id      = module.vpc.vpc_id
+  ports       = [22, 80, 443]
+  cidr_blocks = ["0.0.0.0/0"]
+  environment = var.environment
+}
+
+module "ec2" {
+  source             = "../../modules/ec2"
+  ami_id             = "ami-0c02fb55956c7d316" # Replace with your desired AMI ID
+  instance_type      = "t2.micro"
+  subnet_id          = module.vpc.public_subnet_ids[0] # Attach to an existing public subnet
+  security_group_id  = module.security_group.id        # Use an existing security group
+  key_name           = "linuxkey"                     # Specify the SSH key
+  associate_public_ip = true
+
+  tags = {
+    Environment = var.environment
+  }
+  environment = var.environment
+}
+
--- terraform_infra/modules/vpc/main.tf
+++ terraform_infra/modules/vpc/main.tf
@@ -23,3 +23,22 @@ resource "aws_internet_gateway" "main" {
     Name = "${var.environment}-igw"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
