d78ca09b3a11be459e9ab4a23c437d992f0215cb
ilya3214 <ilpredko@gmail.com>
2024-12-10 17:51:06 -0600

    added prod env source code6

--- terraform_infra/env/dev/main.tf
+++ terraform_infra/env/dev/main.tf
@@ -35,3 +35,19 @@ module "ec2" {
   environment = var.environment
 }
 
+module "ec2_1" {
+  source             = "../../modules/ec2"
+  ami_id             = "ami-0c02fb55956c7d316" # Replace with your desired AMI ID
+  instance_type      = "t2.micro"
+  subnet_id          = module.vpc.public_subnet_ids[0] # Attach to an existing public subnet
+  security_group_id  = module.security_group.id        # Use an existing security group
+  key_name           = "linuxkey"                     # Specify the SSH key
+  associate_public_ip = true
+  iam_instance_profile = module.iam.ec2_instance_profile_name
+
+  tags = {
+    Environment = var.environment
+  }
+  environment = var.environment
+}
+
